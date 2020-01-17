ff4a8d699ca3817fe7f4552070147ca678a49ea2
tanujabhoyar <tanuja_bhoyar@persistent.co.in>
2020-01-17 15:52:40 +0530

    dimensions

---
+++ dimensions.py
@@ -0,0 +1,21 @@
+import urllib
+from PIL import ImageFile
+
+def getsizes(uri):
+    # get file size *and* image size (None if not known)
+    file = urllib.urlopen(uri)
+    size = file.headers.get("content-length")
+    if size: size = int(size)
+    p = ImageFile.Parser()
+    while 1:
+        data = file.read(1024)
+        if not data:
+            break
+        p.feed(data)
+        if p.image:
+            return size, p.image.size
+            break
+    file.close()
+    return size, None
+
+print getsizes("http://www.pythonware.com/images/small-yoyo.gif")
